b5b1e62e3c0c7825b04bdc9e7108998069558f89
AlexRuMath <nmg2017@yandex.ru>
2022-05-02 13:15:55 +0300

    update: refactor

--- swagger-autogen/src/parser/classes/Elements/Url.js
+++ swagger-autogen/src/parser/classes/Elements/Url.js
@@ -4,13 +4,27 @@ module.exports = class Url {
     constructor(path, options) {
         this.path = path.replace("'", "");
         this.params = [];
-        pathRegexp.pathToRegexp(path, this.params);
+        
+        this.parseParams();
+        this.parseGroup(options);
+    }
+
+    parseParams()
+    {
+        pathRegexp.pathToRegexp(this.path, this.params);
         this.params.forEach((param) => {
             this.path = this.path.replace(":" + param.name, "{" + param.name + "}");
         })
+    }
 
+    parseGroup(options)
+    {
         const match = pathRegexp.match(options.url.templateRout); //TODO Test
-        let dataFromPaths = match(path);
-        this.controller = dataFromPaths.params[options.url.groupBy];
+        let dataFromPaths = match(this.path);
+        if (dataFromPaths) {
+            this.controller = dataFromPaths.params[options.url.groupBy];
+        } else {
+            this.controller = "Common";
+        }
     }
 }
