281a5c52e2b6133378064a901a968430fcaaa72d
AlexRuMath <nmg2017@yandex.ru>
2022-05-02 12:27:35 +0300

    fix: problem Object

--- swagger-autogen/src/common/options.js
+++ swagger-autogen/src/common/options.js
@@ -1,12 +1,14 @@
 class Options {
     constructor(options) {
+        this.pathDoc = "./swagger.json";
+        this.docApi = '/api-docs';
         this.url = {
             templateRout: "/api/:version/:controller/:resources+",
             groupBy: "controller",
         }
 
         if (options) {
-            for (let [name, config] of Obejct.entries(options)) {
+            for (let [name, config] of Object.entries(options)) {
                 this[name] = config;
             }
         }
