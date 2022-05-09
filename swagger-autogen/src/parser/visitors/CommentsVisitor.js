86f109ba55d0ae051f34f9d7d639627cb2d65701
AlexRuMath <nmg2017@yandex.ru>
2022-05-09 18:46:27 +0300

    add: check children

--- swagger-autogen/src/parser/visitors/CommentsVisitor.js
+++ swagger-autogen/src/parser/visitors/CommentsVisitor.js
@@ -4,59 +4,58 @@ const tags = require("../classes/Tags");
 
 module.exports = class CommentsVisitor extends SwaggerParserVisitor {
 
-	constructor()
-	{
+	constructor() {
 		super();
 		this.tags = {};
 	}
 
 	// Visit a parse tree produced by SwaggerParser#swaggerDocument.
 	visitSwaggerDocument(ctx) {
-        return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
 
 	// Visit a parse tree produced by SwaggerParser#swaggerElements.
 	visitSwaggerElements(ctx) {
-	  return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
 
 	// Visit a parse tree produced by SwaggerParser#swaggerElement.
 	visitSwaggerElement(ctx) {
 		this.addTag(ctx);
-        return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
 
 	// Visit a parse tree produced by SwaggerParser#swaggerContent.
 	visitSwaggerContent(ctx) {
-	  return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
 
 	// Visit a parse tree produced by SwaggerParser#swaggerAttribute.
 	visitSwaggerAttribute(ctx) {
-        return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
 
 	// Visit a parse tree produced by SwaggerParser#swaggerChardata.
 	visitSwaggerChardata(ctx) {
-	  return this.visitChildren(ctx);
+		return this.visitChildren(ctx);
 	}
 
-	addTag(ctx)
-	{
-        let tag = ctx.getChild(1).getText();
-        let content = ctx.swaggerContent().getText();
-        let createTag = {};
+	addTag(ctx) {
+		if(ctx.children === null) return;
+
+		let tag = ctx.getChild(1).getText();
+		let content = ctx.swaggerContent().getText();
+		let createTag = {};
 		let parameters = ctx.swaggerAttribute() || null;
 
 		createTag = new tags[tag](content, parameters);
-		
-		if(!this.tags[tag])
-		{
+
+		if (!this.tags[tag]) {
 			this.tags[tag] = [];
 		}
 
