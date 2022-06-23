e01d84e8d15fa5200e7e8f6f85c4bb4f3fdc271c
AlexRuMath <nmg2017@yandex.ru>
2022-06-23 12:11:03 +0300

    update: api-request

--- src/runner/api/api-request.js
+++ src/runner/api/api-request.js
@@ -5,12 +5,19 @@ class ApiRequest {
     constructor() {
         this.endpoint = "";
         this.method = "";
-        this.parameters = [];
+        this.parameters = {};
     }
 
     async send(baseUrl) {
         let apiResponse = new ApiResponse();
-        apiResponse.response = await axios[this.method](baseUrl + this.endpoint);
+        let requestConfig = {
+            method: this.method,
+            baseUrl: baseUrl,
+            url: this.endpoint,
+            headers: { 'ContentType': 'application/json' },
+            ...this.parameters.example
+        }
+        apiResponse.response = await axios(requestConfig);
         
         return apiResponse;
     }
