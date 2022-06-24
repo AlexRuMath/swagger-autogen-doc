cd36824bc921517cf18a9ecd14d464fb108eb248
AlexRuMath <nmg2017@yandex.ru>
2022-06-24 16:58:13 +0300

    update: README

--- src/listeners/run.js
+++ src/listeners/run.js
@@ -3,10 +3,10 @@ const addSchemeResponse = require("../utils/add-response-scheme");
 const fs = require('fs');
 
 module.exports = function (pathToDoc) {
-    return async function () {
+    return async function (port) {
         let swaggerDoc = JSON.parse(fs.readFileSync(pathToDoc));
         let requests = createApiRequests(swaggerDoc);
-        let runner = new Runner(requests);
+        let runner = new Runner(requests, port);
         let responses = await runner.run();
         responses.map(response => addSchemeResponse(swaggerDoc, response));
         let json = JSON.stringify(swaggerDoc);
